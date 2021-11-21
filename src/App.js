82ef224996bda40b8dca6dc6e272a04e5025dec1
Jeon <abc8339265@gmail.com>
2021-11-21 18:51:15 +0900

    ch5 complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Say from './Say';
+import EventPractice from "./component/EventPractice";
+import ValidationSample from "./ValidationSample";
+import ScrollBox from "./component/ScrollBox";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <ScrollBox />;
+};
 
 export default App;
