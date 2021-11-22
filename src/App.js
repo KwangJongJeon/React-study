b2b7e937acee250582a5da997eba02057b13d26e
Jeon <abc8339265@gmail.com>
2021-11-22 10:17:12 +0900

    ch6 complete

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import Say from './Say';
 import EventPractice from "./component/EventPractice";
 import ValidationSample from "./ValidationSample";
 import ScrollBox from "./component/ScrollBox";
+import IterationSample from "./component/ch6/IterationSample";
 
 const App = () => {
-  return <ScrollBox />;
+  return (
+      <IterationSample/>
+  )
 };
 
 export default App;
